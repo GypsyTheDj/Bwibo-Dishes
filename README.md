7d46937fca4bc88aff17b0285f8385b37723db71
GypsyTheDj <gypsythedj@gmail.com>
2020-12-23 00:21:13 +0300

    Iterations

--- README.md
+++ README.md
@@ -1,5 +1,38 @@
-| COOK | 🇰🇪
-Welcome to my art👩🏽‍🍳😊
-📞: 0713775108 to order or for any enquiries
-🚫: Closed on Mondays only
-📩: bwibobusiness95@gmail.com
+<h1>Name : Bwibo Dishes Order App</h1>
+
+<hr/>
+<h3>Project Features</h3>
+<ol>
+  <li>Intro Sliders for New Users</li>
+  <li>User Registration and SignIn</li>
+  <li>Firebase Authentication</li>
+  <li>Menu and Food List Loading</li>
+  <li>Different types of food under a menu item</li>
+  <li>Details about food and add to Cart option</li>
+  <li>Give Order for food</li>
+  <li>Orders Management Process</li>
+  <li>Place an order which is stored in the SQLite Database and after that submitted to Firebase</li>
+  <li>Display order details</li>
+  
+  ● Using Firebase to store restaurant data and applying MVC Design Pattern for the Client side & Server side.
+
+</ol>
+
+<hr/>
+<h3>Development Tools & Technologies</h3>
+<ul>
+  <li>Java</li>
+  <li>XML</li>
+  <li>Firebase</li>
+  <li>Google Location Services</li>
+  <li>Google Maps</li>
+</ul>
+
+</hr>
+<h3>Database</h3>
+<ul>
+  <li>SQLite</li>
+  <li>Firebase</li>
+</ul>
+
+<hr/>
---
+++ app/src/main/java/com/normannuthu/bwibodishes/OnboardingActivity.java
@@ -0,0 +1,30 @@
+package com.normannuthu.bwibodishes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    ImageView logo, appName, splashImg;
+    LottieAnimationView lottieAnimationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+
+        logo = findViewById(R.id.logo);
+        appName = findViewById(R.id.app_name);
+        splashImg = findViewById(R.id.img);
+        lottieAnimationView = findViewById(R.id.lottie);
+
+        splashImg.animate().translationY(-1600).setDuration(1000).setStartDelay(4000);
+        logo.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        appName.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+    }
+}
